ce5b259d8bccb08e355e4b93ee1f7b48b7cc84cf
Benton Westergaard <benton.westergaard@gmail.com>
2020-11-13 20:04:06 +0000

    Done.

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,1 +1,9 @@
-# Write your solution here
+# Write your solution herechance_of_rain = 0.2
+current_time = Time.now
+current_time = current_time.to_i
+ 
+if current_time.even?
+  puts "Even!"
+else
+  puts "Odd!"
+end
